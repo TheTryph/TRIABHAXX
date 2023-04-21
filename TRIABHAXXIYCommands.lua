0b89bb77654f33bb33430f56d1886f77b452e85c
TheTryph <ihab020907@gmail.com>
2023-04-21 04:41:38 +0000

    Update TRIABHAXXIYCommands.lua

--- TRIABHAXXIYCommands.lua
+++ TRIABHAXXIYCommands.lua
@@ -4,6 +4,24 @@ local companyInfo = {
     Date = game:HttpGet("https://raw.githubusercontent.com/TheTryph/TRIABHAXX/main/additionalInfo/companyDate.txt")
 }
 
+local installedMacros = {
+    ["project_beatdown"] = function()
+        function getStaminaPercent()
+            local gui = game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.BarFrame.StaminaFrame.Clip
+            return gui.Size.X.Scale * 100
+        end
+
+        local tool = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+
+        while tool.Parent == game.Players.LocalPlayer.Character do
+            if getStaminaPercent() > 99 then
+                game:GetService("VirtualUser"):ClickButton1(Vector2.new(.5, .5))
+            end
+            wait(1)
+        end
+    end
+}
+
 local Plugin = {
     ["PluginName"] = companyInfo.Name .. " commands",
     ["PluginDescription"] = "made by " .. companyInfo.Name .. " " .. companyInfo.Date .. ", " .. ver,
@@ -12,7 +30,7 @@ local Plugin = {
             ["ListName"] = "islandsUI / isUI",
             ["Description"] = "executes " .. companyInfo.Name .. " island hack",
             ["Aliases"] = {"islandsUI", "isUI"},
-            ["Function"] = function(args,speaker)
+            ["Function"] = function(args, speaker)
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/TheTryph/TRIABHAXX/main/Source/islands.lua"))()
             end
         },
@@ -20,14 +38,35 @@ local Plugin = {
             ["ListName"] = "telekinesis / tel",
             ["Description"] = "FE telekinesis script hack, source from https://www.mastersmzscripts.com",
             ["Aliases"] = {"telekinesis", "tel"},
-            ["Function"] =  function(args, speaker)
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/TheTryph/TRIABHAXX/main/Source/telekinesis.lua"))()
+            ["Function"] = function(args, speaker)
+                loadstring(game:HttpGet(
+                    "https://raw.githubusercontent.com/TheTryph/TRIABHAXX/main/Source/telekinesis.lua"))()
+            end
+        },
+        ["macro"] = {
+            ["ListName"] = "macro / mcr",
+            ["Description"] = "Executes various macros, check the current macros by using the command 'macros'",
+            ["Aliases"] = {"macro", "mcr"},
+            ["Function"] = function(args, speaker)
+                installedMacros.args[1]()
+            end
+        },
+        ["getMacro"] = {
+            ["ListName"] = "macros / gmcr",
+            ["Description"] = "Shows you a list of currently available macros.",
+            ["Aliases"] = {"macros", "gmcr"},
+            ["Function"] = function(args, speaker)
+                local stringToShow = "Currently installed macros: "
+                for i,v in pairs(installedMacros) do
+                    stringToShow = stringToShow .. i .. ", "
+                end
+                notify(stringToShow)
             end
         },
         ["createClouds"] = {
             ["ListName"] = "clouds / clds (CLIENT)",
             ["Description"] = "Creates ROBLOX's new volumetric clouds.",
-            ["Aliases"] = {"clouds" , "clds"},
+            ["Aliases"] = {"clouds", "clds"},
             ["Function"] = function(args, speaker)
                 if not workspace.Terrain:FindFirstChild("Clouds") then
                     Instance.new("Clouds", workspace.Terrain)
@@ -83,7 +122,8 @@ local Plugin = {
             ["Aliases"] = {"setcldscolor"},
             ["Function"] = function(args, speaker)
                 if workspace.Terrain:FindFirstChild("Clouds") then
-                    workspace.Terrain.Clouds.Color = Color3.fromRGB(tonumber(args[1]), tonumber(args[2]), tonumber(args[3]))
+                    workspace.Terrain.Clouds.Color = Color3.fromRGB(tonumber(args[1]), tonumber(args[2]),
+                        tonumber(args[3]))
                     notify("Clouds color successfully set!")
                 else
                     notify("There are no clouds.")
@@ -146,7 +186,9 @@ local Plugin = {
             ["description"] = "Information about the plugin",
             ["Aliases"] = {"tinfo"},
             ["Function"] = function(args, speaker)
-                notify("Commands plugin made by " .. companyInfo.Name .. " " .. companyInfo.Date .. ", plugin version: " .. ver .. ", Discord community server coming soon.")
+                notify(
+                    "Commands plugin made by " .. companyInfo.Name .. " " .. companyInfo.Date .. ", plugin version: " ..
+                        ver .. ", Discord community server coming soon.")
             end
         }
     }
